41b7e360a64c61c450cd4a2515dbd5c66ea47536
unknown <albinxavier@gmail.com>
2026-08-21 12:45:58 +0530

    notes

--- Spring AI Projects/tools-calling/src/main/java/com/example/AIController.java
+++ Spring AI Projects/tools-calling/src/main/java/com/example/AIController.java
@@ -4,11 +4,14 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.tools.WeatherAppTool;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@CrossOrigin
 public class AIController {
 
     private final ChatClient chatClient;
